7733b6db5b91c65725af9030dd45a089c2980cea
Pro-me3us <106105250+Pro-me3us@users.noreply.github.com>
2022-06-05 18:36:02 -0400

    Update build_uboot_config.sh

--- build_uboot_config.sh
+++ build_uboot_config.sh
@@ -17,6 +17,6 @@ UBOOT_IMAGES="fip/_tmp/u-boot.bin"
 #       gcc-linaro-aarch64-none-elf-4.8 (aarch64-none-elf compiler) and
 #       CodeSourcery g++ lite (arm-none-eabi compiler)
 ################################################################################
-export PATH="$PATH:/opt/toolchains/gcc-linaro-aarch64-none-elf-4.8-2013.11_linux/bin"
-export PATH="$PATH:/opt/toolchains/gcc-linaro-arm-none-eabi-4.8-2013.11_linux/bin"
+export PATH="$PATH:/opt/toolchains/gcc-linaro-aarch64-none-elf-4.8-2013.11_linux/bin" //edit with path to your build
+export PATH="$PATH:/opt/toolchains/gcc-linaro-arm-none-eabi-4.8-2013.11_linux/bin"    //edit with path to your build
 
